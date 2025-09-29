c465a8ad67daa81d273181e578419e2e619c4d54
Tiffany <mituya1884tansan@gamil.com>
2025-09-30 02:02:18 +0900

    Test

---
+++ src/app/coinLaundry/[id]/page.jsx
@@ -0,0 +1,11 @@
+import Mono from "@/app/components/Mono";
+
+const CoinLaundry = async({params}) => {
+    const{id}=await params;
+    const coinLaundryId=id;
+    return(
+       <Mono id={coinLaundryId}/>
+    )
+}
+
+export default CoinLaundry;
--- src/app/components/List.jsx
+++ src/app/components/List.jsx
@@ -1,5 +1,6 @@
 "use client"
 import {useState,useEffect} from 'react'
+import Link from 'next/link'
 
 const List = () =>{
     
@@ -11,17 +12,23 @@ const List = () =>{
             const response =await fetch('/api/coinLaundry');
             const result=await response.json();
             if(result.success){
-                setCoinLaundries(result.data)
+                setCoinLaundries(result.data);
             }
+            setLoading(false);
         };
         fetchCoinLaundryStories();
     },[])
   
     return(
         <>
+         {loading && <div>読み込み中...</div>}
         <ul>
-            {coinLaundries.map(coinLaundry=>{
-                return <li key={coinLaundry._id}>{coinLaundry.store}</li>
+            {coinLaundries.map(coinLaundry=>{ 
+                return (
+                        <li key={coinLaundry._id}>せんたくランド{coinLaundry.store}店
+                            <Link href={`/coinLaundry/${coinLaundry._id}`} >もっと見る</Link>
+                        </li>
+                        )
             })}
         </ul>
       
