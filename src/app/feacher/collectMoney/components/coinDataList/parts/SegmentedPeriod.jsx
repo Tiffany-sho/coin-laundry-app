bb4b12d3d3436ab78a51ba1cee2c57c36ea4ff7c
Tiffany-sho <mituya1884tansan@gmail.com>
2026-04-12 22:08:16 +0900

    VStackのimport漏れを修正

--- src/app/feacher/collectMoney/components/coinDataList/parts/SegmentedPeriod.jsx
+++ src/app/feacher/collectMoney/components/coinDataList/parts/SegmentedPeriod.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Box, Button, HStack, Slider, Text } from "@chakra-ui/react";
+import { Box, Button, HStack, Slider, Text, VStack } from "@chakra-ui/react";
 import { LuChevronLeft, LuChevronRight } from "@/app/feacher/Icon";
 import { useUploadPage } from "@/app/feacher/collectMoney/context/UploadPageContext";
 import {
