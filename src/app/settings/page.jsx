5992518b2576aee1ca41513d62a8c92c0489db37
Tiffany-sho <mituya1884tansan@gmail.com>
2026-05-28 01:30:20 +0900

    feat(subscription): add PlanCard to settings page

--- src/app/settings/page.jsx
+++ src/app/settings/page.jsx
@@ -3,18 +3,20 @@ export const dynamic = "force-dynamic";
 import { Box, VStack, HStack, Heading } from "@chakra-ui/react";
 import { getUser } from "@/app/api/supabaseFunctions/supabaseDatabase/user/action";
 import { getProfile } from "@/app/api/supabaseFunctions/supabaseDatabase/profiles/action";
-import { getMyOrganization } from "@/app/api/supabaseFunctions/supabaseDatabase/organization/action";
+import { getMyOrganization, getOrgPlan } from "@/app/api/supabaseFunctions/supabaseDatabase/organization/action";
 import AccountInfoCard from "@/app/feacher/settings/components/AccountInfoCard";
 import OrgInfoCard from "@/app/feacher/settings/components/OrgInfoCard";
 import AppSettingsCard from "@/app/feacher/settings/components/AppSettingsCard";
 import OtherActionsCard from "@/app/feacher/settings/components/OtherActionsCard";
+import PlanCard from "@/app/feacher/settings/components/PlanCard";
 import * as Icon from "@/app/feacher/Icon";
 
 export default async function SettingsPage() {
   const { user } = await getUser();
-  const [{ data: profile }, { data: org }] = await Promise.all([
+  const [{ data: profile }, { data: org }, { data: planInfo }] = await Promise.all([
     getProfile(),
     getMyOrganization(),
+    getOrgPlan(),
   ]);
 
   return (
@@ -29,6 +31,7 @@ export default async function SettingsPage() {
       <VStack align="stretch" gap={4}>
         <AccountInfoCard user={user} profile={profile} myRole={org?.myRole} />
         {org?.myRole === "admin" && <OrgInfoCard org={org} />}
+        {org?.myRole === "admin" && planInfo && <PlanCard planInfo={planInfo} />}
         <AppSettingsCard collectMethod={profile?.collectMethod} />
         <OtherActionsCard />
       </VStack>
